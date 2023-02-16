c2fcca3f5af1249f3c5ccae5b325cb2bfbfcbaa0
Aktan Sanhal <aktansanhal@gmail.com>
2023-02-17 02:58:14 +0300

    backend completed

---
+++ back-end/movies/src/main/java/com/aktansanhal/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.aktansanhal.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
